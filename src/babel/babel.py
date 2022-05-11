954ac3e784368dfd3ce3597f6a2969acc484aeab
Simon Paulsson <simon@paulsson.one>
2022-05-11 20:54:09 +0200

    refactored into new Page class

--- src/babel/babel.py
+++ src/babel/babel.py
@@ -1,77 +1,93 @@
 import requests
 import string
 from bs4 import BeautifulSoup
+from dataclasses import dataclass
 
 from . import utils
 
 
-class Library:
-	"""
-	Class for interacting with the library
-	"""
+# CONSTANTS
+URL = "https://libraryofbabel.info"
+GET_PAGE_URL = "/book.cgi"
 
-	# URL to the library
-	url = "https://libraryofbabel.info"
 
-	GET_PAGE = "/book.cgi"
+@dataclass
+class Page:
+	"""A class for a page"""
+	hexagon: str
+	wall: int
+	shelf: int
+	volume: int
+	page: int
 
-	def __init__(self):
-		pass
+	def valid_location(self):
+		return (
+			Page.valid_hexagon(self.hexagon) and 
+			Page.valid_wall(self.wall) and
+			Page.valid_shelf(self.shelf) and 
+			Page.valid_volume(self.volume) and
+			Page.valid_page(self.page)
+		)
 
-	def get_page(self, hexagon: str, wall: int, shelf: int, volume: int, page: int) -> str:
-		"""
-		Get a page and return the contents of the page
-
-		hex: str
-		wall: int [1-4]
-		shelf: int [1-5]
-		volume: int [1-32]
-		page: int [1-410]
-		"""
-
-		if not self.valid_hexagon(hexagon):
-			raise ValueError(f"Hexagon '{hexagon}'' is not valid. It should only contain a-z or 0-9")
-		if not self.valid_wall(wall):
-			raise ValueError(f"Wall '{wall}' is not valid. It should be an int in the range 1-4")
-		if not self.valid_shelf(shelf):
-			raise ValueError(f"Shelf '{shelf}' is not valid. It shoud be an int int the range 1-5")
-		if not self.valid_volume(volume):
-			raise ValueError(f"Volume '{volume}' is not valid. It shoud be an int int the range 1-32")
-		if not self.valid_page(page):
-			raise ValueError(f"Page '{page}' is not valid. It shoud be an int int the range 1-410")
-
-		request_url = self.url + self.GET_PAGE
-		form = {
-			'hex': hexagon,
-			'wall': wall,
-			'shelf': shelf,
-			'volume': volume,
-			'page': page
-		}
-
-		response = requests.post(request_url, data=form)
-
-		soup = BeautifulSoup(response.text, features="html.parser")
-
-		return soup.find(id="textblock").get_text()
-
-		
-
-	def valid_hexagon(self, hexagon: str) -> bool:
+	@staticmethod
+	def valid_hexagon(hexagon: str) -> bool:
 		"""Only abc...xyz and 0-9 are allowed and it should not be empty"""
 		return hexagon and utils.string.contains_only(hexagon, string.ascii_lowercase + string.digits)
 
-	def valid_wall(self, wall: int) -> bool:
+	@staticmethod
+	def valid_wall(wall: int) -> bool:
 		return utils.math.isint(wall) and utils.math.inrange(wall, 1, 4)
 
-	def valid_shelf(self, shelf: int) -> bool:
+	@staticmethod
+	def valid_shelf(shelf: int) -> bool:
 		return utils.math.isint(shelf) and utils.math.inrange(shelf, 1, 5)
 
-	def valid_volume(self, volume: int) -> bool:
+	@staticmethod
+	def valid_volume(volume: int) -> bool:
 		return utils.math.isint(volume) and utils.math.inrange(volume, 1, 32)
 
-	def valid_page(self, page: int) -> bool:
+	@staticmethod
+	def valid_page(page: int) -> bool:
 		return utils.math.isint(page) and utils.math.inrange(page, 1, 410)
+
+	@classmethod
+	def from_dict(clc, page_dict: dict):
+		return clc(*page_dict)
+
+	def to_dict(self) -> dict:
+		return {
+			'hex': self.hexagon,
+			'wall': self.wall,
+			'shelf': self.shelf,
+			'volume': self.volume,
+			'page': self.page
+		}
+
+	def __str__(self):
+		hexagon_str = self.hexagon if len(self.hexagon) <= 10 else self.hexagon[:5] + '...' + self.hexagon[-5:]
+		return f"{hexagon_str}-w{self.wall}-s{self.shelf}-v{self.volume}:{self.page}"
+
+
+def get_page(page: Page) -> str:
+	"""
+	Get a page and return the contents of the page
+	"""
+
+	if not page.valid_location():
+		raise InvalidPageException(f"{page} is not a valid page")
+
+	request_url = URL + GET_PAGE_URL
+	form = page.to_dict()
+
+	response = requests.post(request_url, data=form)
+
+	soup = BeautifulSoup(response.text, features="html.parser")
+
+	return soup.find(id="textblock").get_text()
+
+		
+class InvalidPageException(Exception):
+	pass
 
 
 
